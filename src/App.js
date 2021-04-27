f844f4cfe366800e36008ba6578842449247f4ac
cleber-teixeira <cleberlt@gmail.com>
2021-04-27 12:50:12 -0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import React from 'react';
 import './styles/App.css';
-import rockGlass from './images/rockGlass.svg';
-import BottomMenu from './components/BottomMenu';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Login from './components/Login';
+// import rockGlass from './images/rockGlass.svg';
+// import BottomMenu from './components/BottomMenu';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-      <BottomMenu />
-    </div>
+    <Router>
+      <Route exact path="/" component={ Login } />
+    </Router>
+    // <div className="meals">
+    //   <span className="logo">TRYBE</span>
+    //   <object
+    //     className="rocksGlass"
+    //     type="image/svg+xml"
+    //     data={ rockGlass }
+    //   >
+    //     Glass
+    //   </object>
+    //   <BottomMenu />
+    // </div>
   );
 }
 
