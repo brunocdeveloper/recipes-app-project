a04577640a682ff20fec32823de253f439eda0a8
cleber-teixeira <cleberlt@gmail.com>
2021-04-28 12:16:17 -0300

    Update

--- src/components/FooterSpec.js
+++ src/components/FooterSpec.js
@@ -1,5 +1,6 @@
 import '../styles/FooterSpec.css';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import drinkIcon from '../images/drinkIcon.svg';
 import exploreIcon from '../images/exploreIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
@@ -7,30 +8,30 @@ import mealIcon from '../images/mealIcon.svg';
 function FooterSpec() {
   return (
     <footer className="footer" data-testid="footer">
-      <object
-        className="cocktailIcon"
-        data={ drinkIcon }
-        data-testid="drinks-bottom-btn"
-        type="image/svg+xml"
-      >
-        Drinks
-      </object>
-      <object
-        className="explorerIcon"
-        data={ exploreIcon }
-        data-testid="explore-bottom-btn"
-        type="image/svg+xml"
-      >
-        Explore
-      </object>
-      <object
-        className="forkIcon"
-        data={ mealIcon }
-        data-testid="food-bottom-btn"
-        type="image/svg+xml"
-      >
-        Meal
-      </object>
+      <Link to="/bebidas">
+        <img
+          alt="drink icon"
+          className="cocktailIcon"
+          data-testid="drinks-bottom-btn"
+          src={ drinkIcon }
+        />
+      </Link>
+      <Link to="/">
+        <img
+          alt="explore icon"
+          className="explorerIcon"
+          data-testid="explore-bottom-btn"
+          src={ exploreIcon }
+        />
+      </Link>
+      <Link to="/comidas">
+        <img
+          alt="meal icon"
+          className="mealIcon"
+          data-testid="food-bottom-btn"
+          src={ mealIcon }
+        />
+      </Link>
     </footer>
   );
 }
